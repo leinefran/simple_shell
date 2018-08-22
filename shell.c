bc72c4b8316376231467c9ac2945714d8940f22b
thenicopixie <391@holbertonschool.com>
2018-08-22 07:02:17 +0000

    simple shell function. Added call for string compare and exit

--- shell.c
+++ shell.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-	unsigned int pid, ppid;
+//	unsigned int pid, ppid;
 	ssize_t read = 0;
 	char *buff = NULL, *str = NULL, **arr;
 	size_t size = 0;
-	int tok_size = 0, result = 0;
-	pid_t c_pid;
+	int /*status, */ex, tok_size = 0/*, result = 0*/;
+	pid_t c_pid = 0;
 
-	pid = get_pid();
-	ppid = get_ppid();
-	printf("pid = %u\nppid = %u\n", pid, ppid);
+//	pid = get_pid();
+//	ppid = get_ppid();
+	//printf("pid = %u\nppid = %u\n", pid, ppid);
 	while (1)
 	{
 		write(STDIN_FILENO, "$ ", 2);
@@ -24,13 +24,13 @@ int main(void)
 			free(buff);
 			return (-1);
 		}
-		printf("read = %li\n", read);
+	//	printf("read = %li\n", read);
 		if (buff && buff[0] != '\n')
 		{
 			str = stringdup(buff);
-			printf("str = %s", str);
+	//		printf("str = %s\n", str);
 			tok_size = toksize(buff);
-			printf("tok_size = %d\n", tok_size);
+	//		printf("tok_size = %d\n", tok_size);
 			if (tok_size == -1)
 				break;
 			arr = malloc(sizeof(char*) * (tok_size + 1));
@@ -39,9 +39,15 @@ int main(void)
 			arr[tok_size] = NULL;
 
 			arr = tokanize(str, arr);
-			printf("arr = %s\n", *arr);
+	//		printf("arr = %s\n", *arr);
 
-			result = few(c_pid, arr);
+			ex = exit_shell(arr, tok_size);
+
+			if (ex == 0)
+				exit(0);
+
+			/*result = */
+			few(c_pid, arr);
 		}
 		free(arr);
 		free(str);
