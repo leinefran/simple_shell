28014e3abfe7fadc879f41afe9229925167062fc
Francesca Cantor <fpcantor@gmail.com>
2018-08-22 23:57:49 +0000

    Added _which function to shell header

--- shell.h
+++ shell.h
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 unsigned int get_pid();
 unsigned int get_ppid();
@@ -14,10 +15,8 @@ char *stringdup(const char *str);
 int toksize(char *str);
 char **tokanize(char *str, char **arr);
 int few(pid_t c_pid, char **arr);
-<<<<<<< HEAD
-=======
 int str_comp (char **arr);
 int exit_shell(char **arr, int tok_num);
->>>>>>> 1d1c86171bad59db14b4a2056d290033ee4b279b
+int _which(char *file);
 
 #endif /* _SHELL_H_ */
