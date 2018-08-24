92e0f323b963016c4296add6c4f4ed83cf5b7d34
Francesca Cantor <fpcantor@gmail.com>
2018-08-24 17:23:16 +0000

    Changed integer parameter name from tok_num to size

--- exit.c
+++ exit.c
@@ -3,15 +3,17 @@
 /**
  * exit_shell - function to exit shell if string compare returns 0
  * @arr: array of string pointers
- * @tok_num: size of array
+ * @size: size of array
  * Return: 0 or -1
  */
-int exit_shell(char **arr, int tok_num)
+int exit_shell(char **arr, int size)
 {
 	int result = 0;
 
 	if (tok_num != 1)
 		return (-1);
+
 	result = str_comp(arr);
+
 	return (result);
 }
