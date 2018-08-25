69bc97be3efc1b630de157ac143a0d72b99b416e
leinefran <penny.leine@gmail.com>
2018-08-25 21:49:47 +0000

    Deleted: unecessary variables. Added: parenthesis to write

--- environ.c
+++ environ.c
@@ -2,18 +2,20 @@
 
 /**
  * print_env - a function that prints the environment.
+ * Return: 0.
  */
 
-void print_env(void)
+int print_env(void)
 {
-	int i, j, length;
+	int i = 0;
 
 	while (environ[i])
 	{
 		/* prints in form of "variable=value" */
-		write(1, environ[i], _strlen(environ[i]);
+		write(1, environ[i], _strlen(environ[i]));
 		i++;
 		write(1, "\n", 1);
 	}
+	return (0);
 }
 
