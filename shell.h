ede8e8097eb17b9b89bd0e67ba5b997d993f672d
leinefran <penny.leine@gmail.com>
2018-08-25 00:09:47 +0000

    Added: structure, and extra prototypes

--- shell.h
+++ shell.h
@@ -8,6 +8,21 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+
+/**
+ * struct environ_list - singly linked list
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct environ_list
+{
+        char *str;
+        struct environ_list *next;
+} env_list;
 
 int _strlen(char *s);
 unsigned int get_pid();
@@ -18,6 +33,11 @@ char **tokenize(char *str);
 int few(pid_t c_pid, char **arr);
 int str_comp (char **arr);
 int exit_shell(char **arr, int size);
+char *_getenv(const char *name);
 int _which(char *file);
+env_list *path(char *command, env_list **head);
+char *str_concat(char *s1, char *s2);
+
+
 
 #endif /* _SHELL_H_ */
