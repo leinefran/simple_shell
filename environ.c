9afb720391c156a9c63ce558089e33f72e46e768
leinefran <penny.leine@gmail.com>
2018-08-25 21:01:18 +0000

    Fixed: betty style. Changed: function prototype

--- environ.c
+++ environ.c
@@ -1,15 +1,19 @@
 #include "shell.h"
 
-int print_env()
+/**
+ * print_env - a function that prints the environment.
+ */
+
+void print_env(void)
 {
 	int i, j, length;
 
-	while(environ[i])
+	while (environ[i])
 	{
-		// prints in form of "variable=value"
-		write (1, environ[i], _strlen(environ[i]));
+		/* prints in form of "variable=value" */
+		write(1, environ[i], _strlen(environ[i]);
 		i++;
-		write (1, "\n", 1);
+		write(1, "\n", 1);
 	}
 }
 
