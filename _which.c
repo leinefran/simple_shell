08d65f1328e42789f1d0fd1a105357633827a0b9
leinefran <penny.leine@gmail.com>
2018-08-25 21:04:53 +0000

    Changed: return value. Deleted: else statement and unecessary variable

--- _which.c
+++ _which.c
@@ -3,24 +3,20 @@
 /**
  * _which - This function checks whether a file exists in PATH
  * @file: The file to check
- * Returns: On success, 0. Otherwise, -1.
+ * Return: On success, 0. Otherwise, -1.
  */
 
 int _which(char *file)
 {
-	unsigned int i = 0;
 	struct stat status;
 
-	while(file)
+	while (file)
 	{
 		if (stat(file, &status) == 0)
 		{
 			return (0);
 		}
-		else
-		{
-			return (-1);
-		}
+		break;
 	}
-	return (1);
+	return (-1);
 }
