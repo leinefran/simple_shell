f34e5c96427fb52b5c51caa72ce4e9bee7a97c6a
Francesca Cantor <fpcantor@gmail.com>
2018-08-28 04:12:56 +0000

    Added error function to handle outputting custom error messages

--- error.c
+++ error.c
@@ -2,11 +2,32 @@
 
 /**
  * error - This function returns custom error messages
- * @str - The argument at index 0
+ * @str: The argument at index 0
+ * @line: Line count from getline function
+ * @flag: To determine which error message to output
  */
 
-void error(char *str)
+void error(char *str, int line, int flag)
 {
-        char *shell = _getenv("_");
-        printf("%s: 1: %s: not found\n", shell, str);
+	char *line_counter = _itoa(line);
+	char *shell = _getenv("_");
+	char *error_msg = str_concat(shell, ": ", flag);
+
+	/* concatenate error message */
+	error_msg = str_concat(error_msg, line_counter, flag);
+	error_msg = str_concat(error_msg, ": ", flag);
+	error_msg = str_concat(error_msg, str, flag);
+
+	/* error message dependent on situation */
+	if (flag == 2) /* permission denied */
+		error_msg = str_concat(error_msg,
+": Permission denied\n", flag);
+	else if (flag == 0) /* fie not found */
+		error_msg = str_concat(error_msg, ": not found\n", flag);
+	else /* malloc error */
+		error_msg = str_concat(error_msg, " : TEST\n", flag);
+
+	write(1, error_msg, _strlen(error_msg));
+
+	free(error_msg);
 }
